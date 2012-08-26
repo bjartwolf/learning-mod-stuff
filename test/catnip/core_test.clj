0457a5706236c8a9885e398a7491e6270d608b59
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-08-26 23:11:20 +0200

    seemed to have misunderstood something is testing not

--- test/catnip/core_test.clj
+++ test/catnip/core_test.clj
@@ -3,13 +3,22 @@
         catnip.core
         clojure.math.numeric-tower))
 
+(deftest fermatthree-test
+  (testing "Testing fermats theorem"
+    (let [p 13
+          a 4
+          b 10]
+    (is ((congruent p)  (expt a p) a))
+    (is ((congruent p)  (expt b p) b))
+    (is ((congruent p)  (expt 21 p) 21))
+)))
 
-(deftest a-simple-test
+(deftest a-test
   (testing "Testing congruency"
     (is ((congruent 5)  -8 7))
-    (not ((congruent 5)  -7 7 ))
+    (is (not ((congruent 5)  -7 7 )))
     (is ((congruent 12)  38 14))
-    (not ((congruent 12)  38 13))
+    (is (not ((congruent 12)  38 13)))
     (is ((congruent 5)  2 -3)) 
     (is ((congruent 5) -3 -8))
 ))
@@ -20,13 +29,13 @@
     (is ((congruent p)  (expt 2 (- p 1)) 1)) 
     (is ((congruent p)  (expt 3 (- p 1)) 1)) 
     (is ((congruent p)  (expt 4 (- p 1)) 1))
-    (not ((congruent p)  (expt 5 (- p 1)) 1))
-    (not ((congruent p)  (expt 10 (- p 1)) 1))
+    (is (not ((congruent p)  (expt 5 (- p 1)) 1)))
+    (is (not ((congruent p)  (expt 10 (- p 1)) 1)))
     (is ((congruent p)  (expt 15 (- p 1)) 0))
     (is ((congruent p)  (expt 25 (- p 1)) 0))
 )))
 
-(deftest fermat-two-test
+(deftest fermattwo-test
   (testing "Testing fermats little theorem for another prime"
     (let [p 7]
     (is ((congruent p)  (expt 2 (- p 1)) 1)) 
@@ -34,20 +43,11 @@
     (is ((congruent p)  (expt 4 (- p 1)) 1))
     (is ((congruent p)  (expt 5 (- p 1)) 1))
     (is ((congruent p)  (expt 6 (- p 1)) 1))
-    (not ((congruent p)  (expt 7 (- p 1)) 1))
+    (is (not ((congruent p)  (expt 7 (- p 1)) 1)))
     (is ((congruent p)  (expt 8 (- p 1)) 1))
-    (not ((congruent p)  (expt 14 (- p 1)) 1))
-    (not ((congruent p)  (expt 21 (- p 1)) 1))
+    (is (not ((congruent p)  (expt 14 (- p 1)) 1)))
+    (is (not ((congruent p)  (expt 21 (- p 1)) 1)))
 )))
 
 
 
-(deftest fermat-three-test
-  (testing "Testing fermats theorem"
-    (let [p 13
-          a 4
-          b 10]
-    (is ((congruent p)  (expt a p) a))
-    (is ((congruent p)  (expt b p) b))
-    (is ((congruent p)  (expt 21 p) 21))
-)))
