f4e9f2421da932212cf847dc24a0d89999f2cf43
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-08-26 22:30:21 +0200

    more fermat tests

--- test/catnip/core_test.clj
+++ test/catnip/core_test.clj
@@ -12,11 +12,25 @@
     (is ((congruent 5)  2 -3)) 
     (is ((congruent 5) -3 -8))))
 
-(deftest fermat
+(deftest fermat-test
   (testing "Testing fermats little theorem"
     (let [p 5]
     (is ((congruent p)  (expt 2 (- p 1)) 1)) 
     (is ((congruent p)  (expt 3 (- p 1)) 1)) 
     (is ((congruent p)  (expt 4 (- p 1)) 1))
     (not ((congruent p)  (expt 5 (- p 1)) 1))
+)))
+
+(deftest fermat2-test
+  (testing "Testing fermats little theorem for another prime"
+    (let [p 7]
+    (is ((congruent p)  (expt 2 (- p 1)) 1)) 
+    (is ((congruent p)  (expt 3 (- p 1)) 1)) 
+    (is ((congruent p)  (expt 4 (- p 1)) 1))
+    (is ((congruent p)  (expt 5 (- p 1)) 1))
+    (is ((congruent p)  (expt 6 (- p 1)) 1))
+    (not ((congruent p)  (expt 7 (- p 1)) 1))
+    (is ((congruent p)  (expt 8 (- p 1)) 1))
+    (not ((congruent p)  (expt 14 (- p 1)) 1))
+    (not ((congruent p)  (expt 21 (- p 1)) 1))
 )))
