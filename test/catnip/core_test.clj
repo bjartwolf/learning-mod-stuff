5a62f7f86e02eed79f6824a8dae3c7ab04065d19
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-08-26 20:00:55 +0200

    updated tests

--- test/catnip/core_test.clj
+++ test/catnip/core_test.clj
@@ -4,9 +4,9 @@
 
 (deftest a-test
   (testing "Testing congruency"
-    (is (= ((congruent 5)  -8 7) true))
-    (not (= ((congruent 5)  -7 7) true))
-    (is (= ((congruent 12)  38 14) true))
-    (not (= ((congruent 12)  38 13) true))
-    (is (= ((congruent 5)  2 -3) true)) 
-    (is (= ((congruent 5) -3 -8) true))))
+    (is ((congruent 5)  -8 7))
+    (not ((congruent 5)  -7 7 ))
+    (is ((congruent 12)  38 14))
+    (not ((congruent 12)  38 13))
+    (is ((congruent 5)  2 -3)) 
+    (is ((congruent 5) -3 -8))))
