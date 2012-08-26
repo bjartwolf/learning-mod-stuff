a256f59ae08071da1db937293e7e673f044304a4
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-08-26 22:21:42 +0200

    testing fermats little theorem

--- test/catnip/core_test.clj
+++ test/catnip/core_test.clj
@@ -1,6 +1,7 @@
 (ns catnip.core-test
   (:use clojure.test
-        catnip.core))
+        catnip.core
+        clojure.math.numeric-tower))
 
 (deftest a-test
   (testing "Testing congruency"
@@ -10,3 +11,12 @@
     (not ((congruent 12)  38 13))
     (is ((congruent 5)  2 -3)) 
     (is ((congruent 5) -3 -8))))
+
+(deftest fermat
+  (testing "Testing fermats little theorem"
+    (let [p 5]
+    (is ((congruent p)  (expt 2 (- p 1)) 1)) 
+    (is ((congruent p)  (expt 3 (- p 1)) 1)) 
+    (is ((congruent p)  (expt 4 (- p 1)) 1))
+    (not ((congruent p)  (expt 5 (- p 1)) 1))
+)))
