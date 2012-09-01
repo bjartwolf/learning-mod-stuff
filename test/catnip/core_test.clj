df6512810b2e3d35dc2edfdf72de9523bf9962cb
Bjørn Einar Bjartnes <bjartwolf@bjartwolf-desktop.(none)>
2012-09-02 00:25:06 +0200

    updated test descr

--- test/catnip/core_test.clj
+++ test/catnip/core_test.clj
@@ -3,27 +3,23 @@
         catnip.core
         clojure.math.numeric-tower))
 
-(deftest fermatthree-test
-  (testing "Testing fermats theorem"
-    (let [p 13
-          a 4
-          b 10]
-    (is ((congruent p)  (expt a p) a))
-    (is ((congruent p)  (expt b p) b))
-    (is ((congruent p)  (expt 21 p) 21))
-)))
+(deftest congruency-test
+  (testing "Explaining basic congruency"
+    (is ((congruent 5) 5 10))
+    (is (not ((congruent 5) 5 11)))
+    (is ((congruent 12)  38 14))
+    (is (not ((congruent 12) 38 15)))
+))
 
-(deftest a-test
-  (testing "Testing congruency"
+(deftest congruency-of-neg-numbers-test
+  (testing "Congruency of negative numbers"
     (is ((congruent 5)  -8 7))
     (is (not ((congruent 5)  -7 7 )))
-    (is ((congruent 12)  38 14))
-    (is (not ((congruent 12)  38 13)))
     (is ((congruent 5)  2 -3)) 
     (is ((congruent 5) -3 -8))
 ))
 
-(deftest fermat-test
+(deftest fermat-little-theorem-test-1
   (testing "Testing fermats little theorem"
     (let [p 5]
     (is ((congruent p)  (expt 2 (- p 1)) 1)) 
@@ -35,7 +31,18 @@
     (is ((congruent p)  (expt 25 (- p 1)) 0))
 )))
 
-(deftest fermattwo-test
+
+(deftest fermat-little-theorem-test-2
+  (testing "Testing fermats theorem"
+    (let [p 13
+          a 4
+          b 10]
+    (is ((congruent p)  (expt a p) a))
+    (is ((congruent p)  (expt b p) b))
+    (is ((congruent p)  (expt 21 p) 21))
+)))
+
+(deftest fermat-little-theorem-test-3
   (testing "Testing fermats little theorem for another prime"
     (let [p 7
           congruent-7 (congruent p)]
@@ -49,6 +56,3 @@
     (is (not (congruent-7 (expt 14 (- p 1)) 1)))
     (is (not (congruent-7 (expt 21 (- p 1)) 1)))
 )))
-
-
-
